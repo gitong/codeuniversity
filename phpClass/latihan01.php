4290f746c052050109bac89d665cb21fea7b3c24
gitong <gitong@gmail.com>
2013-10-09 20:53:20 +0700

    commit ke empat
    fokus ngasih comment

--- phpClass/latihan01.php
+++ phpClass/latihan01.php
@@ -1,4 +1,5 @@
 <?php
 echo "what ever it takes!";
 echo "ini yang kedua!";
+//ternyata susah juga kalo ngeliat dari yang sini. Membedaknya gak semudah itu. Dan itu akan jadi hal yang menantang untuk membuat itu susah.
 ?>
