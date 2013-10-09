99cadba31bba4e7aee0f805ccd40b8bf3e22045e
gitong <gitong@gmail.com>
2013-10-09 21:09:19 +0700

    ini nyoba di branches
    bisa disini

--- phpClass/latihan01.php
+++ phpClass/latihan01.php
@@ -2,4 +2,6 @@
 echo "what ever it takes!";
 echo "ini yang kedua!";
 //ternyata susah juga kalo ngeliat dari yang sini. Membedaknya gak semudah itu. Dan itu akan jadi hal yang menantang untuk membuat itu susah.
+echo 5+19;
+// operator matematika
 ?>
