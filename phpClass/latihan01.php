16bce939901bf914f48c68997c17025ad12f43f8
gitong <gitong@gmail.com>
2013-10-09 21:15:56 +0700

    ini cabang berikutnya

--- phpClass/latihan01.php
+++ phpClass/latihan01.php
@@ -2,4 +2,7 @@
 echo "what ever it takes!";
 echo "ini yang kedua!";
 //ternyata susah juga kalo ngeliat dari yang sini. Membedaknya gak semudah itu. Dan itu akan jadi hal yang menantang untuk membuat itu susah.
+echo "test yang lain!";
+// jauh banget yang lain lagi
+echo "test banget galau";
 ?>
