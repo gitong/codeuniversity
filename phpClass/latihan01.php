7352f575e3ed285a2c3c87cf1300ac93cd3244bb
gitong <gitong@gmail.com>
2013-10-12 07:29:07 +0700

    test head saat merging
    ini juga ama test twitter api

--- phpClass/latihan01.php
+++ phpClass/latihan01.php
@@ -2,12 +2,12 @@
 echo "what ever it takes!";
 echo "ini yang kedua!";
 //ternyata susah juga kalo ngeliat dari yang sini. Membedaknya gak semudah itu. Dan itu akan jadi hal yang menantang untuk membuat itu susah.
-<<<<<<< HEAD
+//<<<<<<< HEAD
 echo "test yang lain!";
 // jauh banget yang lain lagi
 echo "test banget galau";
-=======
+//=======
 echo 5+19;
 // operator matematika
->>>>>>> cabang01
+//	>>>>>>> cabang01
 ?>
